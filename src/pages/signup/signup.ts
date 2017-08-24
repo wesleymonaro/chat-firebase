e1c786278896ca8d83e9f7e602c756f06ddc33d8
Wesley Monaro <wesleymonaro@hotmail.com>
2017-08-24 00:14:11 -0300

    Cadastrando usuario para autenticaçao

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,7 +1,10 @@
+import { User } from './../../models/user.model';
+import { AuthService } from './../../providers/auth.service';
 import { UserService } from './../../providers/user.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FirebaseAuthState } from "angularfire2";
 
 
 @Component({
@@ -10,13 +13,14 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 })
 export class SignupPage {
 
-  signupForm : FormGroup;
+  signupForm: FormGroup;
 
   constructor(
-    public formBuilder : FormBuilder,
-    public navCtrl: NavController, 
+    public authService: AuthService,
+    public formBuilder: FormBuilder,
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public userService : UserService
+    public userService: UserService
   ) {
 
     let emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
@@ -24,17 +28,24 @@ export class SignupPage {
     this.signupForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       username: ['', [Validators.required, Validators.minLength(3)]],
-      email : ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])],
-      password : ['', [Validators.required, Validators.minLength(6)]]
+      email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     })
   }
 
-  onSubmit() : void {
-    this.userService.create(this.signupForm.value)
-      .then(() => {
-        console.log("usuario cadastrado");
-      })
+  onSubmit(): void {
 
+    let user: User = this.signupForm.value;
+
+    this.authService.createAuthUser({
+      email: user.email,
+      password: user.password
+    }).then((authState: FirebaseAuthState) => {
+      this.userService.create(this.signupForm.value)
+        .then(() => {
+          console.log("usuario cadastrado");
+        })
+    })
   }
 
 }
