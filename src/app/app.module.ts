a989531bc70d9bc094d1d1cf494811300a137c23
Wesley Monaro <wesleymonaro@hotmail.com>
2017-08-23 23:19:48 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+
+const firebaseAppConfig : FirebaseAppConfig = {
+    apiKey: "AIzaSyCvI6cdRxsFtQei70islw2HioAiN4t3PKo",
+    authDomain: "ionic2-firebase-chat-7e814.firebaseapp.com",
+    databaseURL: "https://ionic2-firebase-chat-7e814.firebaseio.com",
+    storageBucket: "ionic2-firebase-chat-7e814.appspot.com",
+    messagingSenderId: "867278605639"
+  };
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAppConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
