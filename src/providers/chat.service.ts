9f2edc46abffa3a488f46f9667af9391ffd7f43e
Wesley Monaro <wesleymonaro@hotmail.com>
2017-08-25 23:13:51 -0300

    Lista de chats no chatService

--- src/providers/chat.service.ts
+++ src/providers/chat.service.ts
@@ -1,4 +1,5 @@
-import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { FirebaseAuthState } from 'angularfire2';
+import { AngularFire, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
 import { Chat } from './../models/chat.model';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
@@ -9,10 +10,31 @@ import { BaseService } from "./base.service";
 @Injectable()
 export class ChatService extends BaseService {
 
+  chats : FirebaseListObservable<Chat[]>;
+
   constructor(
     public af : AngularFire
   ) {
     super();
+    this.setChats();
+  }
+
+  private setChats():void{
+    this.af.auth.subscribe((authState : FirebaseAuthState) => {
+      
+      if(authState){
+
+        this.chats = <FirebaseListObservable<Chat[]>>this.af.database.list(`/chats/${authState.auth.uid}`, {
+          query : {
+            orderByChild : 'timestamp'
+          }
+        }).map((chats : Chat[]) => {
+          return chats.reverse();
+        }).catch(this.handleObservableError);
+
+      }
+
+    })
   }
 
   create(chat : Chat, userId1 : string, userId2 : string) : firebase.Promise<void>{
