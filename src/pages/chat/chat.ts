0c70cb003889e0de1b6460972f8ca7927a1ea9d1
Wesley Monaro <wesleymonaro@hotmail.com>
2017-08-26 00:05:57 -0300

    Lista de mensagens na chatPage

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,4 +1,7 @@
+import { MessageService } from './../../providers/message.service';
+import { FirebaseListObservable } from 'angularfire2';
 import { UserService } from './../../providers/user.service';
+import { Message } from './../../models/message.model';
 import { User } from './../../models/user.model';
 import { AuthService } from './../../providers/auth.service';
 import { Component } from '@angular/core';
@@ -11,35 +14,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ChatPage {
 
-  messages : string[] = [];
-  pageTitle : string;
-  sender : User;
-  recipient : User;
+  messages: FirebaseListObservable<Message[]>;
+  pageTitle: string;
+  sender: User;
+  recipient: User;
 
   constructor(
-    public authService : AuthService,
-    public navCtrl: NavController, 
+    public authService: AuthService,
+    public messageService: MessageService,
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public userService : UserService
+    public userService: UserService
   ) {
   }
 
-  ionViewCanEnter(): Promise<boolean>{
+  ionViewCanEnter(): Promise<boolean> {
     return this.authService.authenticated;
   }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     this.recipient = this.navParams.get('recipientUser');
     this.pageTitle = this.recipient.name;
     this.userService.currentUser
       .first()
-      .subscribe((currentUser : User) => {
-        this.sender = currentUser;
+      .subscribe((currentUser: User) => {
+        this.messages = this.messageService
+          .getMessages(this.sender.$key, this.recipient.$key);
+
+        this.messages
+          .first()
+          .subscribe((messages: Message[]) => {
+            if (messages.length == 0) {
+              this.messages = this.messageService
+                .getMessages(this.recipient.$key, this.sender.$key);
+            }
+          })
       })
   }
 
-  sendMessage(newMessage : string) : void{
+  sendMessage(newMessage: string): void {
     this.messages.push(newMessage);
-  } 
+  }
 
 }
