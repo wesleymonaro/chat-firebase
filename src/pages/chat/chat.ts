ec99e548f65b7c0688e1ea6411826149d9e16ddc
Wesley Monaro <wesleymonaro@hotmail.com>
2017-08-25 22:46:49 -0300

    Título do chat

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,3 +1,5 @@
+import { UserService } from './../../providers/user.service';
+import { User } from './../../models/user.model';
 import { AuthService } from './../../providers/auth.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -10,16 +12,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class ChatPage {
 
   messages : string[] = [];
+  pageTitle : string;
+  sender : User;
+  recipient : User;
 
   constructor(
     public authService : AuthService,
     public navCtrl: NavController, 
-    public navParams: NavParams
+    public navParams: NavParams,
+    public userService : UserService
   ) {
   }
 
   ionViewCanEnter(): Promise<boolean>{
     return this.authService.authenticated;
+  }
+
+  ionViewDidLoad(){
+    this.recipient = this.navParams.get('recipientUser');
+    this.pageTitle = this.recipient.name;
+    this.userService.currentUser
+      .first()
+      .subscribe((currentUser : User) => {
+        this.sender = currentUser;
+      })
   }
 
   sendMessage(newMessage : string) : void{
