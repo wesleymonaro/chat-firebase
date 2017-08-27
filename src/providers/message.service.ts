53334c8462c67df717985f4830369b97a1ca921d
Wesley Monaro <wesleymonaro@hotmail.com>
2017-08-26 23:09:25 -0300

    Limitando quantidade de mensagens

--- src/providers/message.service.ts
+++ src/providers/message.service.ts
@@ -8,23 +8,24 @@ import { BaseService } from "./base.service";
 
 
 @Injectable()
-export class MessageService extends BaseService{
+export class MessageService extends BaseService {
 
   constructor(
-    public af : AngularFire
+    public af: AngularFire
   ) {
     super();
   }
 
-  create(message : Message, listMessages : FirebaseListObservable<Message[]>) : firebase.Promise<void>{
+  create(message: Message, listMessages: FirebaseListObservable<Message[]>): firebase.Promise<void> {
     return listMessages.push(message)
       .catch(this.handlePromiseError);
   }
 
-  getMessages(userId1, userId2) : FirebaseListObservable<Message[]>{
-    return <FirebaseListObservable<Message[]>> this.af.database.list(`/messages/${userId1}-${userId2}`, {
-      query : {
-        orderByChild : 'timestamp'
+  getMessages(userId1, userId2): FirebaseListObservable<Message[]> {
+    return <FirebaseListObservable<Message[]>>this.af.database.list(`/messages/${userId1}-${userId2}`, {
+      query: {
+        orderByChild: 'timestamp',
+        limitToLast: 50
       }
     }).catch(this.handleObservableError)
   }
